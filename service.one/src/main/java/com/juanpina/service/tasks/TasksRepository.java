d577f3e07ffdf19e7a5927edc302b2e0019c24e3
Juan Pina <juanpinaribeiro@outlook.com>
2025-03-10 14:07:16 -0300

    Crianco conexão com o banco h2 e corrigindo algumas incosistências

--- service.one/src/main/java/com/juanpina/service/tasks/TasksRepository.java
+++ service.one/src/main/java/com/juanpina/service/tasks/TasksRepository.java
@@ -2,13 +2,15 @@ package com.juanpina.service.tasks;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface TasksRepository extends JpaRepository<TasksEntity, Long> {
 
-    @Query("select t from TasksEntity t where t.dueDate <= :deadline and t.notified = false")
+    @Query("SELECT t FROM Task t WHERE t.dueDate <= :deadline AND t.notified = FALSE")
     List<TasksEntity> findTasksDueWithinDeadline(LocalDateTime deadline);
 
 }
